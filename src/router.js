fc424cb4876990c2ef2bcce0ee882c520f2571e2
wh-cl <83022251+BlackDigital-Web@users.noreply.github.com>
2024-11-09 14:53:13 +0100

    Lien catégories fonctionnels + recherche de boucle pour afficher que le lien acceuil sur les categories

--- src/router.js
+++ src/router.js
@@ -5,4 +5,22 @@ const router = Router();
 
 router.get('/', appController.index);
 
+router.get('/categorie/:name', (req, res) =>{
+    const nameUrl = req.params.name;
+    const category = [
+        'fruits',
+        'legumes',
+    ]
+    const foundName = category.find(name =>
+        name.toLowerCase() === nameUrl.toLowerCase()
+        )
+
+    if(foundName === 'fruits'){
+        res.render('fruits');
+    }else {
+        res.render('legumes');
+    }
+})
+    
+
 export {router};
